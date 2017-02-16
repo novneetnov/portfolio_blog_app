2da5298aa9a07be4c5cd31dda8b67df22dd1d32f
novneetnov <novneet346iith@gmail.com>
2017-02-16 21:44:35 +0530

    Implement create, edit & update functionalities

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,51 @@
 class PortfoliosController < ApplicationController
+	before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
+	
 	def index
 		@portfolio_items = Portfolio.all
 	end
+
+	def new
+		@portfolio_item = Portfolio.new
+	end
+
+	def create
+		@portfolio_item = Portfolio.new(portfolio_params)
+		respond_to do |format|
+			if @portfolio_item.save
+				format.html { redirect_to @portfolio_item, notice: "Portfolio Item created successfully" }
+			else
+				format.html { render :new }
+			end
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		respond_to do |format|
+			if @portfolio_item.update
+				format.html { redirect_to @portfolio_item, notice: "Portfolio Item updated successfully" }
+			else
+				format.html { render :edit }
+			end
+		end
+	end
+
+	def destroy
+	end
+
+	private
+	def portfolio_params
+		params.require(:portfolio).permit(:title, :subtitle, :body)
+	end
+
+	def set_portfolio_item
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+
 end
