fc4d9e47477c112d40da10157bc0036556439b01
novneetnov <novneet346iith@gmail.com>
2017-03-08 12:34:08 +0530

    Refactor auth links to a view helper for all Layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def login_helper
+		if current_user.is_a? User
+			link_to "Logout", destroy_user_session_path, method: :delete
+		else
+			signup = link_to "Signup", new_user_registration_path
+			login = link_to "Login", new_user_session_path
+			signup + " | " + login
+		end
+	end
 end
